dc2cea646a2c5faa4c9e9c5ccdad298b3e598b3e
Mehul Mohan <mehulmpt@gmail.com>
2020-09-06 02:44:46 +0530

    GitHub Actions enabled

---
+++ .github/workspaces/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+user="ubuntu"
+
+rm -rf .git
+rm -rf .gitignore
+git config --global user.email "mehulmpt@gmail.com"
+git config --global user.name "Mehul Mohan"
+mv .gitignore_cicd .gitignore
+git init .
+git add .
+git commit -m "Deploying"
+git remote add production ssh://$user@$AWS_HOST/~/codedamn/graphql
+git push --force production master
+
+# ssh $user@$AWS_HOST "cd ~/codedamn/graphql && \
+# docker-compose -f ./docker/compose/common.yml -f ./docker/compose/prod.yml build && \
+# docker-compose -f ./docker/compose/common.yml -f ./docker/compose/prod.yml up --detach && \
+# exit"
